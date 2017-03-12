91c03b035b01c1e401c9990f956e6fbbddbce292
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-03-12 21:09:46 +0100

    Verify hash bytewise

--- build.rs
+++ build.rs
@@ -24,10 +24,15 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
+// We use the diceware list of the EFF, which contains 7776 common English words between three and
+// nine characters.  I'm unsure about the licensing situation concerning this list so I'm not
+// shipping it with the sources but instead download it during the build and use its hash to verify
+// its authenticity.
 static URL: &'static str = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt";
-static WORDLIST_SHA256: &'static str = {
-    "addd35536511597a02fa0a9ff1e5284677b8883b83e986e43f15a3db996b903e"
-};
+static WORDLIST_SHA256: &'static [u8] = &[0xad, 0xdd, 0x35, 0x53, 0x65, 0x11, 0x59, 0x7a, 0x02,
+                                          0xfa, 0x0a, 0x9f, 0xf1, 0xe5, 0x28, 0x46, 0x77, 0xb8,
+                                          0x88, 0x3b, 0x83, 0xe9, 0x86, 0xe4, 0x3f, 0x15, 0xa3,
+                                          0xdb, 0x99, 0x6b, 0x90, 0x3e];
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
@@ -44,8 +49,8 @@ fn main() {
 
     let mut hasher = Sha256::new();
     hasher.input(&wordlist_buffer);
-    let hash = hasher.result().map(|b| format!("{:02x}", b)).join("");
-    if hash != WORDLIST_SHA256 {
+    let hash = hasher.result();
+    if *hash != *WORDLIST_SHA256 {
         panic!("SHA256 mismatch for EFF wordlist! Report issue to \
                 https://github.com/lunaryorn/xkpwgen.rs");
     }
