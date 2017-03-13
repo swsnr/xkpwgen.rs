5f34c4003c29175c88897e6633a415f883295ba2
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-03-13 11:30:37 +0100

    Use imperative style for all help messages

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,8 @@ fn main() {
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY, to the extent permitted by law.")
+        .version_message("Print version information")
+        .help_message("Print this message")
         .arg(Arg::with_name("number")
                  .short("n")
                  .long("number")
