c4a1d60b90f2e17e20c2035622f5fad3f2f8ad69
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-03-13 13:08:34 +0100

    Add license and word list stats to --version

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,23 @@ extern crate rand;
 use clap::{App, AppSettings, Arg};
 
 static EFF_WORDLIST: &'static str = include_str!(concat!(env!("OUT_DIR"), "/eff_wordlist.txt"));
+static LICENSE: &'static str = {
+    "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law."
+};
 
 fn parse_diceware_list(input: &str) -> Vec<&str> {
     input.lines().map(|l| l.split_whitespace().last().unwrap()).collect()
 }
 
 fn main() {
-    let matches = App::new("xkpwgen")
+    let parse_result = App::new("xkpwgen")
         .version(crate_version!())
         .about("Generate XKCD 936 passwords")
-        .after_help("Copyright (C) 2017 Sebastian Wiesner <swiesner@lunaryorn.com>
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
-This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.")
+        .after_help(format!("Copyright (C) 2017 Sebastian Wiesner <swiesner@lunaryorn.com>\n\n{}",
+                            LICENSE)
+                            .as_str())
         .version_message("Print version information")
         .help_message("Print this message")
         .arg(Arg::with_name("number")
@@ -53,16 +57,37 @@ There is NO WARRANTY, to the extent permitted by law.")
                     AppSettings::DontCollapseArgsInUsage,
                     // Don't put flags and options in separate --help groups
                     AppSettings::UnifiedHelpMessage])
-        .get_matches();
+        .get_matches_safe();
 
-    let words = parse_diceware_list(EFF_WORDLIST);
-    let password_length = value_t_or_exit!(matches.value_of("length"), usize);
-    let number_of_passwords = value_t_or_exit!(matches.value_of("number"), usize);
-    for _ in 0..number_of_passwords {
-        println!("{}",
-                 rand::sample(&mut rand::thread_rng(),
-                              words.iter().map(|s| *s),
-                              password_length)
-                         .join(" "));
+    match parse_result {
+        Ok(matches) => {
+            let words = parse_diceware_list(EFF_WORDLIST);
+            let password_length = value_t_or_exit!(matches.value_of("length"), usize);
+            let number_of_passwords = value_t_or_exit!(matches.value_of("number"), usize);
+            for _ in 0..number_of_passwords {
+                println!("{}",
+                         rand::sample(&mut rand::thread_rng(),
+                                      words.iter().map(|s| *s),
+                                      password_length)
+                                 .join(" "));
+            }
+        }
+        Err(error @ clap::Error { kind: clap::ErrorKind::VersionDisplayed, .. }) => {
+            let mut words = parse_diceware_list(EFF_WORDLIST);
+            words.sort_by_key(|w| w.chars().count());
+            print!("\n
+EFF long wordlist July 2017: {} words (min length {}, max length {})
+
+{}",
+                   words.len(),
+                   words[0].chars().count(),
+                   words.last()
+                       .unwrap()
+                       .chars()
+                       .count(),
+                   LICENSE);
+            error.exit();
+        }
+        Err(error) => error.exit(),
     }
 }
