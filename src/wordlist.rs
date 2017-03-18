141fa9f28548f4c8d59c7d0e01b832f1e0f0199b
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-03-18 14:37:55 +0100

    Move wordlist tests into wordlist mod

--- src/wordlist.rs
+++ src/wordlist.rs
@@ -20,3 +20,45 @@ static EFF_WORDLIST: &'static str = include_str!(concat!(env!("OUT_DIR"), "/eff_
 pub fn builtin_words() -> Vec<&'static str> {
     EFF_WORDLIST.lines().collect()
 }
+
+#[cfg(test)]
+mod test {
+    use super::builtin_words;
+    use std::collections::HashSet;
+
+    #[test]
+    fn has_7776_words() {
+        assert_eq!(builtin_words().len(), 7776);
+    }
+
+    #[test]
+    fn contains_no_duplicate_words() {
+        let mut seen_words = HashSet::with_capacity(8000);
+        let mut duplicate_words = HashSet::new();
+        for word in builtin_words().into_iter() {
+            if !seen_words.insert(word) {
+                duplicate_words.insert(word);
+            }
+        }
+
+        assert!(duplicate_words.is_empty(),
+                "Duplicate words found: {}",
+                duplicate_words.into_iter().collect::<Vec<_>>().join(" "));
+    }
+
+    #[test]
+    fn contains_no_empty_words() {
+        for word in builtin_words() {
+            assert!(word.len() > 0, "Got empty word");
+        }
+    }
+
+    #[test]
+    fn contains_no_words_with_whitespace() {
+        for word in builtin_words() {
+            assert!(!word.contains(|c: char| c.is_whitespace()),
+                    "Word {} contained whitespace!",
+                    word);
+        }
+    }
+}
