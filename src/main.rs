fa78b84dc24fa7aae80202f359fe3785717e2f37
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-08-06 16:59:23 +0200

    Replace EFF wordlist with skeeto pokerware
    Pokerware is public domain, and contains simpler and more common
    words than the EFF wordlist.

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,45 @@ use rand::{Rng, sample};
 use rand::os::OsRng;
 
 
-/// Provide word lists.
+/// Words to generate passwords from.
+///
+/// We use the [pokerware words][1] from  by Christopher Wellons which he released
+/// to public domain.
+///
+/// His blog post [Introducing the Pokerware Secure Passphrase Generator][2] explains where he
+/// obtained these word lists from.
+///
+/// [1]: https://github.com/skeeto/pokerware
+/// [2]: http://nullprogram.com/blog/2017/07/27/
 mod words {
     lazy_static! {
-        /// The EFF wordlist.
+        /// Formal words.
         ///
-        /// The EFF wordlist contains 7776 common English words between three and nine characters in
-        /// length, at an average of seven characters per word.
+        /// > The “formal” list is derived in part from Google’s Ngram Viewer, with my own
+        /// > additional filters and tweaking. It’s called “formal” because the ngrams come from
+        /// > formal publications and represent more formal kinds of speech.
+        ///
+        /// Source: <http://nullprogram.com/blog/2017/07/27/>
+        pub static ref FORMAL: Vec<&'static str> = include_str!("words/formal.txt")
+            .lines()
+            .collect();
 
-        /// The wordlist is provided free of charge by the EFF and may be freely distributed
-        /// under the [CC BY 3.0 US license](https://www.eff.org/copyright)
-        pub static ref EFF: Vec<&'static str> = include_str!("words/eff_large_diceware.txt").lines().collect();
+        /// Slang words.
+        ///
+        /// > The “slang” list is derived from every reddit comment between December 2005 and May
+        /// > 2017, tamed by the same additional filters. I have this data on hand, so I may as well
+        /// > put it to use. I figured more casually-used words would be easier to remember. Due to
+        /// > my extra filtering, there’s actually a lot of overlap between these lists, so the
+        /// > differences aren’t too significant.
+        ///
+        /// Source: <http://nullprogram.com/blog/2017/07/27/>.
+        ///
+        /// See [A Showerthoughts Fortune File][1] for the source of Reddit comments.
+        ///
+        /// [1]: http://nullprogram.com/blog/2016/12/01/
+        pub static ref SLANG: Vec<&'static str> = include_str!("words/slang.txt")
+            .lines()
+            .collect();
     }
 }
 
@@ -64,22 +92,41 @@ xkpwgen license either of
 * MIT license, <http://opensource.org/licenses/MIT>
 at your option.  There is NO WARRANTY, to the extent permitted by law.";
 
+arg_enum! {
+    /// Which list of words to use.
+    #[derive(Clone, Copy, Debug)]
+    pub enum ListOfWords {
+        Slang,
+        Formal
+    }
+}
+
+fn get_words<'a>(list: ListOfWords) -> &'a Vec<&'static str> {
+    match list {
+        ListOfWords::Slang => &*words::SLANG,
+        ListOfWords::Formal => &*words::FORMAL,
+    }
+}
+
 struct Options<'a> {
     length_of_password: usize,
     number_of_passwords: usize,
     word_separator: &'a str,
+    list_of_words: ListOfWords,
 }
 
 impl<'a> Options<'a> {
     fn from_matches(matches: &'a ArgMatches<'a>) -> clap::Result<Options<'a>> {
         let length = value_t!(matches.value_of("length"), usize)?;
         let number = value_t!(matches.value_of("number"), usize)?;
+        let list_of_words = value_t!(matches.value_of("list_of_words"), ListOfWords)?;
         // Separator has a default value, so we can safely unwrap here!
         let separator = matches.value_of("separator").unwrap();
         Ok(Options {
             length_of_password: length,
             number_of_passwords: number,
             word_separator: separator,
+            list_of_words: list_of_words,
         })
     }
 }
@@ -122,6 +169,14 @@ wordlist copyright (C) 2016 EFF <https://www.eff.org/copyright>",
                 .default_value("4")
                 .help("The number of words in each password"),
         )
+        .arg(
+            Arg::with_name("list_of_words")
+                .short("w")
+                .long("--words")
+                .possible_values(&ListOfWords::variants())
+                .default_value(ListOfWords::variants()[0])
+                .help("The list of words to use to generate a password"),
+        )
         .settings(
             &[
                 AppSettings::DontCollapseArgsInUsage,
@@ -137,7 +192,7 @@ wordlist copyright (C) 2016 EFF <https://www.eff.org/copyright>",
     for _ in 0..options.number_of_passwords {
         let password = generate_password(
             &mut rng,
-            &(*words::EFF),
+            get_words(options.list_of_words),
             options.length_of_password,
             options.word_separator,
         );
