b41b01893ccea78f2b4a8ae187770061e3854998
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-08-06 16:03:07 +0200

    Cache EFF words

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,26 @@
 #[macro_use]
 extern crate clap;
 extern crate rand;
+#[macro_use]
+extern crate lazy_static;
 
 use clap::{AppSettings, Arg, ArgMatches};
 use rand::{Rng, sample};
 use rand::os::OsRng;
 
-// Include the wordlist in the binary
-static EFF_WORDLIST: &str = include_str!("eff_large_diceware_wordlist.txt");
 
-/// Get the list of built-in words.
-///
-/// This function returns the [large diceware wordlist from the EFF][1] ([download][2]).  This list
-/// contains 7776 common English words between 3 and 9 characters long, at an average of 7
-/// characters per word.
-///
-/// The wordlist is freely available under the CC BY 3.0 US license; see
-/// <https://www.eff.org/copyright> for more information about the copyright of the EFF.
-///
-/// [1]: https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases
-/// [2]: https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
-pub fn builtin_words() -> Vec<&'static str> {
-    EFF_WORDLIST.lines().collect()
+/// Provide word lists.
+mod words {
+    lazy_static! {
+        /// The EFF wordlist.
+        ///
+        /// The EFF wordlist contains 7776 common English words between three and nine characters in
+        /// length, at an average of seven characters per word.
+
+        /// The wordlist is provided free of charge by the EFF and may be freely distributed
+        /// under the [CC BY 3.0 US license](https://www.eff.org/copyright)
+        pub static ref EFF: Vec<&'static str> = include_str!("words/eff_large_diceware.txt").lines().collect();
+    }
 }
 
 /// Generate a single password from a wordlist.
@@ -86,7 +85,6 @@ impl<'a> Options<'a> {
 }
 
 fn main() {
-    let words = builtin_words();
     let long_version = format!(
         "{}\n
 
@@ -139,11 +137,10 @@ wordlist copyright (C) 2016 EFF <https://www.eff.org/copyright>",
     for _ in 0..options.number_of_passwords {
         let password = generate_password(
             &mut rng,
-            &words,
+            &(*words::EFF),
             options.length_of_password,
             options.word_separator,
         );
         println!("{}", password);
     }
-
 }
