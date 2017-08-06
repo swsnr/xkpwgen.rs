2dacef04530df13079c8cca9c2b537f85f41f038
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-08-06 17:19:29 +0200

    Move arg parsing into submodule

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ extern crate rand;
 #[macro_use]
 extern crate lazy_static;
 
-use clap::{AppSettings, Arg, ArgMatches};
 use rand::{Rng, sample, thread_rng};
 
 
@@ -82,8 +81,12 @@ where
     sample(&mut rng, words.into_iter().map(AsRef::as_ref), length).join(separator)
 }
 
+/// CLI arguments to this tool.
+mod args {
+    use super::words;
+    use clap::{Result, AppSettings, Arg, ArgMatches};
 
-static LICENSE: &'static str = "\
+    static LICENSE: &'static str = "\
 wordlist license CC BY 3.0 US: <http://creativecommons.org/licenses/by/3.0/us/>.
 
 xkpwgen license either of
@@ -91,108 +94,118 @@ xkpwgen license either of
 * MIT license, <http://opensource.org/licenses/MIT>
 at your option.  There is NO WARRANTY, to the extent permitted by law.";
 
-arg_enum! {
-    /// Which list of words to use.
-    #[derive(Clone, Copy, Debug)]
-    pub enum ListOfWords {
-        Slang,
-        Formal
+    arg_enum! {
+        /// Which list of words to use.
+        #[derive(Clone, Copy, Debug)]
+        pub enum ListOfWords {
+            Slang,
+            Formal
+        }
     }
-}
 
-fn get_words<'a>(list: ListOfWords) -> &'a Vec<&'static str> {
-    match list {
-        ListOfWords::Slang => &*words::SLANG,
-        ListOfWords::Formal => &*words::FORMAL,
+    pub struct Options {
+        pub length_of_password: usize,
+        pub number_of_passwords: usize,
+        pub word_separator: String,
+        pub list_of_words: ListOfWords,
     }
-}
 
-struct Options<'a> {
-    length_of_password: usize,
-    number_of_passwords: usize,
-    word_separator: &'a str,
-    list_of_words: ListOfWords,
-}
-
-impl<'a> Options<'a> {
-    fn from_matches(matches: &'a ArgMatches<'a>) -> clap::Result<Options<'a>> {
-        let length = value_t!(matches.value_of("length"), usize)?;
-        let number = value_t!(matches.value_of("number"), usize)?;
-        let list_of_words = value_t!(matches.value_of("list_of_words"), ListOfWords)?;
-        // Separator has a default value, so we can safely unwrap here!
-        let separator = matches.value_of("separator").unwrap();
-        Ok(Options {
-            length_of_password: length,
-            number_of_passwords: number,
-            word_separator: separator,
-            list_of_words: list_of_words,
-        })
-    }
-}
-
-fn main() {
-    let long_version = format!(
-        "{}\n
+    impl Options {
+        pub fn parse() -> Result<Options> {
+            let long_version = format!(
+                "{}\n
 
 {}",
-        crate_version!(),
-        LICENSE
-    );
-    let matches = app_from_crate!()
-        .after_help(
-            "\
+                crate_version!(),
+                LICENSE
+            );
+            let matches = app_from_crate!()
+                .after_help(
+                    "\
 xkpwgen  copyright (C) 2017 Sebastian Wiesner <swiesner@lunaryorn.com>
 wordlist copyright (C) 2016 EFF <https://www.eff.org/copyright>",
-        )
-        .long_version(long_version.as_str())
-        .version_message("Print version and license information")
-        .help_message("Print this message")
-        .arg(
-            Arg::with_name("separator")
-                .short("s")
-                .long("separator")
-                .default_value(" ")
-                .help("The separator between words in a password"),
-        )
-        .arg(
-            Arg::with_name("number")
-                .short("n")
-                .long("number")
-                .default_value("5")
-                .help("The number of passwords to generate at once"),
-        )
-        .arg(
-            Arg::with_name("length")
-                .short("l")
-                .long("length")
-                .default_value("4")
-                .help("The number of words in each password"),
-        )
-        .arg(
-            Arg::with_name("list_of_words")
-                .short("w")
-                .long("--words")
-                .possible_values(&ListOfWords::variants())
-                .default_value(ListOfWords::variants()[0])
-                .help("The list of words to use to generate a password"),
-        )
-        .settings(
-            &[
-                AppSettings::DontCollapseArgsInUsage,
-                // Don't put flags and options in separate --help groups
-                AppSettings::UnifiedHelpMessage,
-            ],
-        )
-        .get_matches();
+                )
+                .long_version(long_version.as_str())
+                .version_message("Print version and license information")
+                .help_message("Print this message")
+                .arg(
+                    Arg::with_name("separator")
+                        .short("s")
+                        .long("separator")
+                        .default_value(" ")
+                        .help("The separator between words in a password"),
+                )
+                .arg(
+                    Arg::with_name("number")
+                        .short("n")
+                        .long("number")
+                        .default_value("5")
+                        .help("The number of passwords to generate at once"),
+                )
+                .arg(
+                    Arg::with_name("length")
+                        .short("l")
+                        .long("length")
+                        .default_value("4")
+                        .help("The number of words in each password"),
+                )
+                .arg(
+                    Arg::with_name("list_of_words")
+                        .short("w")
+                        .long("--words")
+                        .possible_values(&ListOfWords::variants())
+                        .default_value(ListOfWords::variants()[0])
+                        .help("The list of words to use to generate a password"),
+                )
+                .settings(
+                    &[
+                        AppSettings::DontCollapseArgsInUsage,
+                        // Don't put flags and options in separate --help groups
+                        AppSettings::UnifiedHelpMessage,
+                    ],
+                )
+                .get_matches();
 
-    let options = Options::from_matches(&matches).unwrap_or_else(|e| e.exit());
+            Options::from_matches(&matches)
+        }
+
+        /// Extract our options from matched CLI arguments.
+        fn from_matches<'a>(matches: &'a ArgMatches<'a>) -> Result<Options> {
+            let length = value_t!(matches.value_of("length"), usize)?;
+            let number = value_t!(matches.value_of("number"), usize)?;
+            let list_of_words = value_t!(matches.value_of("list_of_words"), ListOfWords)?;
+            let separator = value_t!(matches.value_of("separator"), String)?;
+            Ok(Options {
+                length_of_password: length,
+                number_of_passwords: number,
+                word_separator: separator,
+                list_of_words: list_of_words,
+            })
+        }
+
+        /// Get the words selected by CLI options.
+        pub fn words<'a>(&self) -> &'a Vec<&'static str> {
+            match self.list_of_words {
+                ListOfWords::Slang => &*words::SLANG,
+                ListOfWords::Formal => &*words::FORMAL,
+            }
+        }
+    }
+}
+
+
+
+fn main() {
+    use args::Options;
+
+    let options = Options::parse().unwrap_or_else(|e| e.exit());
 
     for _ in 0..options.number_of_passwords {
         let password = generate_password(
             &mut thread_rng(),
-            get_words(options.list_of_words),
+            options.words(),
             options.length_of_password,
-            options.word_separator,
+            &options.word_separator,
         );
         println!("{}", password);
     }
