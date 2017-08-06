df2951d33e6111a3b77d62a7ba6feb67b8ec703d
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-08-06 15:27:13 +0200

    Remove lib crate

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,58 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+//! Generate XKCD 936 passwords.
+//!
+//! ![](http://imgs.xkcd.com/comics/password_strength.png)
+//!
+
 #![deny(warnings)]
 
 #[macro_use]
 extern crate clap;
 extern crate atty;
 extern crate rand;
-extern crate xkpwgen;
 
 use clap::{AppSettings, Arg, ArgMatches};
+use rand::{Rng, sample};
 use rand::os::OsRng;
-use xkpwgen::generate_password;
-use xkpwgen::wordlist::builtin_words;
+
+// Include the wordlist in the binary
+static EFF_WORDLIST: &str = include_str!("eff_large_diceware_wordlist.txt");
+
+/// Get the list of built-in words.
+///
+/// This function returns the [large diceware wordlist from the EFF][1] ([download][2]).  This list
+/// contains 7776 common English words between 3 and 9 characters long, at an average of 7
+/// characters per word.
+///
+/// The wordlist is freely available under the CC BY 3.0 US license; see
+/// <https://www.eff.org/copyright> for more information about the copyright of the EFF.
+///
+/// [1]: https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases
+/// [2]: https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
+pub fn builtin_words() -> Vec<&'static str> {
+    EFF_WORDLIST.lines().collect()
+}
+
+/// Generate a single password from a wordlist.
+///
+/// Use the random generator `rng` to randomly draw from the wordlist `words` to generate a
+/// password of the given `length`, and concatenate the resulting words with the `separator`.
+pub fn generate_password<'a, R, W, T>(
+    mut rng: &mut R,
+    words: W,
+    length: usize,
+    separator: &str,
+) -> String
+where
+    R: Rng,
+    W: IntoIterator<Item = &'a T>,
+    T: AsRef<str> + 'a,
+{
+    sample(&mut rng, words.into_iter().map(AsRef::as_ref), length).join(separator)
+}
+
 
 static LICENSE: &'static str = "\
 wordlist license CC BY 3.0 US: <http://creativecommons.org/licenses/by/3.0/us/>.
