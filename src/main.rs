86002412237229e753c963adfd641c367848e240
Sebastian Wiesner <sebastian@swsnr.de>
2019-03-04 13:58:09 +0100

    Combine wordlists

--- src/main.rs
+++ src/main.rs
@@ -25,49 +25,14 @@ use rand::{thread_rng, Rng};
 use structopt;
 use structopt::StructOpt;
 
-/// Words to generate passwords from.
+/// Words used by xkpwgen.
 ///
-/// We use the [pokerware words][1] from  by Christopher Wellons which he released
-/// to public domain.
+/// Combined all pokerware wordlists, from <https://github.com/skeeto/pokerware>.
 ///
-/// His blog post [Introducing the Pokerware Secure Passphrase Generator][2] explains where he
-/// obtained these word lists from.
+/// Wordlists were released to the public domain.
 ///
-/// [1]: https://github.com/skeeto/pokerware
-/// [2]: http://nullprogram.com/blog/2017/07/27/
-mod words {
-    use lazy_static::lazy_static;
-
-    lazy_static! {
-        /// Formal words.
-        ///
-        /// > The “formal” list is derived in part from Google’s Ngram Viewer, with my own
-        /// > additional filters and tweaking. It’s called “formal” because the ngrams come from
-        /// > formal publications and represent more formal kinds of speech.
-        ///
-        /// Source: <http://nullprogram.com/blog/2017/07/27/>
-        pub static ref FORMAL: Vec<&'static str> = include_str!("words/formal.txt")
-            .lines()
-            .collect();
-
-        /// Slang words.
-        ///
-        /// > The “slang” list is derived from every reddit comment between December 2005 and May
-        /// > 2017, tamed by the same additional filters. I have this data on hand, so I may as well
-        /// > put it to use. I figured more casually-used words would be easier to remember. Due to
-        /// > my extra filtering, there’s actually a lot of overlap between these lists, so the
-        /// > differences aren’t too significant.
-        ///
-        /// Source: <http://nullprogram.com/blog/2017/07/27/>.
-        ///
-        /// See [A Showerthoughts Fortune File][1] for the source of Reddit comments.
-        ///
-        /// [1]: http://nullprogram.com/blog/2016/12/01/
-        pub static ref SLANG: Vec<&'static str> = include_str!("words/slang.txt")
-            .lines()
-            .collect();
-    }
-}
+/// See also <http://nullprogram.com/blog/2017/07/27/>.
+const WORDS: &'static str = include_str!("words.txt");
 
 /// Generate a single password from a wordlist.
 ///
@@ -122,19 +87,6 @@ struct Options {
         help = "The separator between words in a password"
     )]
     word_separator: String,
-    #[structopt(long = "slang", help = "Whether to use slang words")]
-    use_slang_words: bool,
-}
-
-impl Options {
-    /// Get the words selected by CLI options.
-    fn words<'a>(&self) -> &'a Vec<&'static str> {
-        if self.use_slang_words {
-            &*words::SLANG
-        } else {
-            &*words::FORMAL
-        }
-    }
 }
 
 fn main() {
@@ -161,10 +113,11 @@ wordlists copyright (C) 2017 Christopher Wellons",
         .get_matches();
     let options = Options::from_clap(&matches);
 
+    let words: Vec<&'static str> = WORDS.lines().collect();
     for _ in 0..options.number_of_passwords {
         let password = generate_password(
             &mut thread_rng(),
-            options.words(),
+            &words,
             options.length_of_password,
             &options.word_separator,
         );
